74d81b6cc496655d4e6c1531ec38b215b47bfeeb
RockatanskyV8 <luccasanttoz@gmail.com>
2020-02-03 19:00:35 -0300

    logica de reconhecimento dos numeros

--- src/app.js
+++ src/app.js
@@ -11,8 +11,44 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
+let unidades = ["", "um", "dois", "três", "quatro", "cinco",
+             "seis", "sete", "oito", "nove", "dez", "onze",
+             "doze", "treze", "quatorze", "quinze", "dezesseis",
+             "dezessete", "dezoito", "dezenove"];
+
+let dezenas = ["", "", "vinte", "trinta", "quarenta", "cinquenta",
+             "sessenta", "setenta", "oitenta", "noventa"]
+
+let centenas = ["", "cento", "duzentos", "trezentos",
+             "quatrocentos", "quinhentos", "seiscentos",
+             "setecentos", "oitocentos", "novecentos"]
+
+let mil = "mil"
+
+let possibilidades = {
+  '1' : (num) => { return unidades[num] },
+  '2' : (num) => {
+                  let alg = num.split("");
+                  let result = "";
+                  if( num < 20 ){
+                    result = unidades[num]
+                  } else if ( num > 20 ){
+                    let num1 = alg[0];
+                    let num2 = alg[1];
+                    result = dezenas[num1] + ' e ' + unidades[num2];
+                  }
+                  return result;
+                },
+  '3' : (num) => {},
+  '4' : (num) => {}
+}
+
 app.use('/:num', (req, res, next) => {
-  res.status(200).send(req.params);
+  let algarismos = (req.params.num).split("")
+  
+  res.status(200).send({
+    'extenso' : possibilidades[ algarismos.length ](req.params.num)
+  });
 });
 
 module.exports = app;
