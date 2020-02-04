6620d409d3bf332c5cbd93d9478f0fb698aedd48
RockatanskyV8 <luccasanttoz@gmail.com>
2020-02-04 15:15:22 -0300

    reconhecendo numeros até 99999

--- src/app.js
+++ src/app.js
@@ -12,40 +12,58 @@ app.use(bodyParser.urlencoded({
 }));
 
 let unidades = ["", "um", "dois", "três", "quatro", "cinco",
-             "seis", "sete", "oito", "nove", "dez", "onze",
-             "doze", "treze", "quatorze", "quinze", "dezesseis",
-             "dezessete", "dezoito", "dezenove"];
+                    "seis", "sete", "oito", "nove", "dez",
+                    "onze", "doze", "treze", "quatorze",
+                    "quinze", "dezesseis", "dezessete",
+                    "dezoito", "dezenove"]
 
-let dezenas = ["", "", "vinte", "trinta", "quarenta", "cinquenta",
-             "sessenta", "setenta", "oitenta", "noventa"]
+let dezenas = ["", "", "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"]
 
-let centenas = ["", "cento", "duzentos", "trezentos",
-             "quatrocentos", "quinhentos", "seiscentos",
-             "setecentos", "oitocentos", "novecentos"]
+let centenas = ["", "cento", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos"]
 
-let mil = "mil"
+let milhares = ["", "", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove"]
+
+function digits2(num){
+  let alg = num.split("");
+  let result = "";
+  if( num < 10 ){
+    result = unidades[ alg[1] ]
+  } else if( num > 10 && num < 20 ){
+    result = unidades[ num ]
+  } else if ( num > 20 ){
+    let num1 = alg[0];
+    let num2 = alg[1];
+    result = dezenas[num1] + ' e ' + unidades[num2];
+  }
+  return result;
+}
 
 let possibilidades = {
   '1' : (num) => { return unidades[num] },
-  '2' : (num) => {
+  '2' : (num) => {return digits2(num)},
+  '3' : (num) => {
                   let alg = num.split("");
-                  let result = "";
-                  if( num < 20 ){
-                    result = unidades[num]
-                  } else if ( num > 20 ){
-                    let num1 = alg[0];
-                    let num2 = alg[1];
-                    result = dezenas[num1] + ' e ' + unidades[num2];
+                  return centenas[ alg[0] ] + ' e '
+                       + digits2([alg[1], alg[2]].join(''))
+                  },
+  '4' : (num) => {
+                  let alg = num.split("");
+                  return milhares[ alg[0] ] + ' mil '
+                       + centenas[ alg[1] ] + ' e '
+                       + digits2([alg[2], alg[3]].join(''))
+                 },
+  '5' : (num) => {
+                  let alg = num.split("");
+                  return digits2([alg[0], alg[1]].join('')) + ' mil '
+                        + centenas[ alg[2] ] + ' e '
+                        + digits2([alg[3], alg[4]].join(''))
                   }
-                  return result;
-                },
-  '3' : (num) => {},
-  '4' : (num) => {}
+
 }
 
 app.use('/:num', (req, res, next) => {
   let algarismos = (req.params.num).split("")
-  
+
   res.status(200).send({
     'extenso' : possibilidades[ algarismos.length ](req.params.num)
   });
